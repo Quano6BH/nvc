81cd74bfccc1e4ff8196bd727f6c119d05f4e523
Bruce-Dewulf <103484113+Bruce-Dewulf@users.noreply.github.com>
2022-08-02 10:08:14 +0700

    initial commit new NVC contract (safeMintTo instead of safeMint)

---
+++ contracts/NextVisionCapital-temp-contract.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./extensions/ERC721AQueryable.sol";
+import "./access/Ownable.sol";
+
+// import "https://github.com/chiru-labs/ERC721A-Upgradeable/blob/main/contracts/ERC721AUpgradeable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/utils/ERC721/extensions/ERC721Enumerable.sol";
+
+interface IERC20 {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+}
+
+contract NextVisionCapital is ERC721AQueryable, Ownable {
+    event NftBurned(address owner, uint256 tokenId, uint256 timestamp);
+
+    uint256 public constant COLLECTION_SIZE = 1000;
+
+    constructor() ERC721A("Next Vision Capital", "NVC") {
+    }
+
+    function baseURI() public pure returns (string memory) {
+        return "ipfs://QmTQqEGEXWncTivYaBwarAYcxadqyy6eg4PCmRfXpS8TAQ/";
+    }
+
+
+    function safeMintTo(address toAddress ,uint256 _quantity) external {
+        require(_quantity > 0, "Quantity must be greater than 0.");
+
+        require(
+            totalSupply() + _quantity <= COLLECTION_SIZE,
+            "Cannot mint over supply cap"
+        );
+
+        _safeMint(toAddress, _quantity);
+    }
+
+
+    function burn(uint256 _tokenId) external onlyOwner {
+        require(ownerOf(_tokenId) == msg.sender, "Not the owner.");
+
+        _burn(_tokenId);
+
+        emit NftBurned(msg.sender, _tokenId, block.timestamp);
+    }
+
+    function burnBatch(uint256[] memory _tokenIds) external onlyOwner {
+        uint256 i = 0;
+
+        for (i; i < _tokenIds.length; i++) {
+            require(ownerOf(_tokenIds[i]) == msg.sender, "Not the owner.");
+
+            _burn(_tokenIds[i]);
+
+            emit NftBurned(msg.sender, _tokenIds[i], block.timestamp);
+        }
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return _totalMinted();
+    }
+
+    function totalBurned() public view returns (uint256) {
+        return _totalBurned();
+    }
+}
