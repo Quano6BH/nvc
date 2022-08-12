e571f28b9ecb18b27081f1c9ba5689e2d6b5d25a
Bruce-Dewulf <103484113+Bruce-Dewulf@users.noreply.github.com>
2022-08-12 16:58:15 +0700

    update database for demo

--- apis/snapshots_gen/snapshotv2_execute/sort.py
+++ apis/snapshots_gen/snapshotv2_execute/sort.py
@@ -3,10 +3,10 @@ with open("snapshot_NVC.txt", "r") as file:
     lines.sort(key=lambda line: int(line.split("|")[0]))
 with open("snapshot_NVC_sorted.txt", "w") as file:
     file.writelines(lines)
-token_ids = [int(x.split("|")[0]) for x in lines]
-count = 0
-for i in range(0, 1000):
-    if i not in token_ids:
-        print(i)
-        count += 1
-print(count)
+# token_ids = [int(x.split("|")[0]) for x in lines]
+# count = 0
+# for i in range(0, 1000):
+#     if i not in token_ids:
+#         print(i)
+#         count += 1
+# print(count)
