1f68d42a2b119fa618a7f01470f92b9a7cef1de3
Bruce-Dewulf <103484113+Bruce-Dewulf@users.noreply.github.com>
2022-08-19 10:09:04 +0700

    remove unecessary files

--- apis/snapshots_gen/change_file_name.py
+++
@@ -1,5 +0,0 @@
-for i in range(1, 32):
-    with open(f"snapshot/2/2022-08/snapshot_WZRDS_2022_8_{i}.txt", "r") as file:
-        data = file.read()
-    with open(f"snapshot/2/2022-08/2_2022-08-{i}.txt", "w") as file:
-        file.write(data)
